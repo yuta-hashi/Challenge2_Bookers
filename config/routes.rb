d2930bb77c11a618f7db816edf6d514cc27607cb
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-10 00:00:40 +0900

    [Start]Challenge2_Bookers

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'books#index'
+end
